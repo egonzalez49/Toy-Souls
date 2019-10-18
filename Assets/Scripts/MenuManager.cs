48f85ed1d5451da4347d9b599a8953b5e635d448
egonzalez49 <egonzalez49@gatech.edu>
2019-10-17 22:01:42 -0400

    Main Menu Scene
    Added a main menu scene with interactivity and sounds.

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    //Start the game whenever the user clicks 'New Game'
+    public void StartGame()
+    {
+        /*
+         * TODO: Add the name of the main scene to load after clicking 'New Game's
+         */
+        //SceneManager.LoadScene("SCENE_NAME");
+    }
+
+    //Close application whenever the user clicks 'Quit'
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
