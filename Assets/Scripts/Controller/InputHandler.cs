ecc37a22ea00effb96ed7f9dd21da7fcf2ce4e1a
mliang1987 <mliang@gatech.edu>
2019-10-18 17:14:32 -0400

    3rd Person Camera and Movement

---
+++ Assets/Scripts/Controller/InputHandler.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OOB
+{
+    public class InputHandler : MonoBehaviour
+    {
+
+        float vertical;
+        float horizontal;
+
+        StateManager states;
+        CameraManager cameraManager;
+
+        float delta;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            states = GetComponent<StateManager>();
+            states.Init();
+            cameraManager = CameraManager.singleton;
+            cameraManager.Init(this.transform);
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            delta = Time.fixedDeltaTime;
+            GetInput();
+            UpdateStates();
+            states.FixedTick(delta);
+            cameraManager.Tick(delta);
+        }
+
+        /*void Update()
+        {
+            delta = Time.deltaTime;
+            
+        }*/
+
+        void GetInput()
+        {
+            vertical = Input.GetAxis("Vertical");
+            horizontal = Input.GetAxis("Horizontal");
+        }
+
+        void UpdateStates()
+        {
+            states.horizontal = horizontal;
+            states.vertical = vertical;
+
+            Vector3 v = vertical * cameraManager.transform.forward;
+            Vector3 h = horizontal * cameraManager.transform.right;
+            states.moveDirection = (v + h).normalized;
+            float m = Mathf.Abs(horizontal) + Mathf.Abs(vertical);
+            states.moveAmount = Mathf.Clamp01(m);
+        }
+    }
+}
---
+++ Assets/Scripts/Controller/StateManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OOB
+{
+    public class StateManager : MonoBehaviour
+    {
+        [Header("Init")]
+        public GameObject activeModel;
+
+        [Header("Inputs")]
+        public float vertical;
+        public float horizontal;
+        public float moveAmount;
+        public Vector3 moveDirection;
+
+        [Header("Stats")]
+        public float moveSpeed;
+        public float runSpeed;
+        public float rotateSpeed;
+
+        [Header("States")]
+        public bool run;
+
+        [HideInInspector]
+        public Animator anim;
+        [HideInInspector]
+        public Rigidbody rigid;
+        [HideInInspector]
+        public float delta;
+       
+        public void Init()
+        {
+            moveSpeed = 3;
+            runSpeed = 4.0f;
+            rotateSpeed = 5;
+            SetupAnimator();
+            rigid = GetComponent<Rigidbody>();
+            rigid.angularDrag = 999;
+            rigid.drag = 4;
+            rigid.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        }
+
+        
+
+        void SetupAnimator()
+        {
+            if (activeModel == null)
+            {
+                anim = GetComponentInChildren<Animator>();
+                if (anim == null)
+                {
+                    Debug.Log("No model found");
+                }
+                else
+                {
+                    activeModel = anim.gameObject;
+                }
+            }
+            if (anim == null)
+                anim = activeModel.GetComponent<Animator>();
+            anim.applyRootMotion = false;
+        }
+
+        public void FixedTick(float d)
+        {
+            delta = d;
+
+            rigid.drag = (moveAmount > 0) ? 0 : 4;
+
+            float targetSpeed = moveSpeed;
+            if (run)
+            {
+                targetSpeed = runSpeed;
+            }
+
+            rigid.velocity = moveDirection * (targetSpeed * moveAmount);
+
+            Vector3 targetDirection = moveDirection;
+            targetDirection.y = 0;
+            if (targetDirection == Vector3.zero)
+                targetDirection = transform.forward;
+            Quaternion tempTR = Quaternion.LookRotation(targetDirection);
+            Quaternion targetRotation = Quaternion.Slerp(transform.rotation, tempTR, delta * moveAmount * rotateSpeed);
+            transform.rotation = targetRotation;
+
+            HandleMovementAnimations();
+        }
+
+        void HandleMovementAnimations()
+        {
+            anim.SetFloat("vertical", moveAmount, 0.4f, delta);
+
+        }
+    }
+}
