1708e94c4299b28f46c38eb59b7b762687205265
Jiyoung Lim <33502257+sam-lim@users.noreply.github.com>
2019-10-19 15:55:32 -0400

    Revert "Merge branch 'master' of https://github.gatech.edu/sliang76/Toys-Souls"
    This reverts commit 98e94fe58405e4af61dc3e46c84fdf75fc11eb40, reversing
    changes made to 2d62618ccd6d5d4d59b91d916d6b0fb041ad9f8d.

--- Assets/Scripts/Survival_Shooter/BossMovement.cs
+++ Assets/Scripts/Survival_Shooter/BossMovement.cs
@@ -22,7 +22,6 @@ namespace Enemy
         public AIState aiState;
         private float dist;
         public float attackDistance = 2.0f;
-        public float chaseSpeed = 1.5f;
 
         private void Awake()
         {
@@ -31,7 +30,6 @@ namespace Enemy
             anim = GetComponent<Animator>();
             playerVelocityReporter = player.GetComponent<VelocityReporter>();
             aiState = AIState.chasePlayer;
-            agent.speed = chaseSpeed;
             dist = Vector3.Distance(player.position, transform.position);
         }
 
@@ -51,8 +49,7 @@ namespace Enemy
 
         void chasePlayer()
         {
-            //SetDestinationMoving();
-            agent.destination = player.position;
+            SetDestinationMoving();
         }
 
         private void SetDestinationMoving()
