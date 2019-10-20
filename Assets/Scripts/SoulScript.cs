321c36f4416b56441f2d685b81b6e49bbfefb383
egonzalez49 <egonzalez49@gatech.edu>
2019-10-20 12:35:38 -0400

    Added Souls Script
    Simply call AddSouls(int n) to change the souls text.

---
+++ Assets/Scripts/SoulScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoulScript : MonoBehaviour
+{
+    public static int souls;
+
+    private Text text;
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        souls = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Souls: " + souls;
+    }
+
+    public void AddSouls(int s)
+    {
+        souls += s;
+    }
+}
