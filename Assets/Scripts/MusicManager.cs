4b36d9e962c387f6ca066d8cfc7204b244f01cb2
egonzalez49 <egonzalez49@gatech.edu>
2019-10-20 11:50:54 -0400

    Added Fade In Music at Start

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public float maxVolume = 0.15f;
+    public float speed = 0.01f;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = 0f;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float audioVolume = audioSource.volume;
+        if (audioVolume < maxVolume)
+        {
+            audioVolume += speed * Time.deltaTime;
+            audioSource.volume = audioVolume;
+        }
+    }
+}
