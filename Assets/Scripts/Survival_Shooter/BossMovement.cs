aec1498aa61117c6e44c9eaf42c4417492435297
mhayes64 <mhayes64@gatech.edu>
2019-10-20 04:19:38 -0400

    Enemy AI Script Updates

--- Assets/Scripts/Survival_Shooter/BossMovement.cs
+++ Assets/Scripts/Survival_Shooter/BossMovement.cs
@@ -12,6 +12,13 @@ namespace Enemy
         NavMeshAgent agent;
         Animator anim;
         private VelocityReporter playerVelocityReporter;
+        private float attackTimer = 1.0f;
+        private bool isPlayerColliding;
+        public bool isAttacking;
+        public float _actionDelay;
+        public float attackDuration;
+        public float attackDistance = 2.0f;
+        public float dot;
 
         public enum AIState
         {
@@ -20,8 +27,8 @@ namespace Enemy
         };
 
         public AIState aiState;
-        private float dist;
-        public float attackDistance = 2.0f;
+        public float dist;
+        
 
         private void Awake()
         {
@@ -31,25 +38,63 @@ namespace Enemy
             playerVelocityReporter = player.GetComponent<VelocityReporter>();
             aiState = AIState.chasePlayer;
             dist = Vector3.Distance(player.position, transform.position);
+            UpdateAnimClipTimes();
         }
 
         // Update is called once per frame
         void Update()
         {
             dist = Vector3.Distance(player.position, transform.position);
-            chasePlayer();
-            /*
-            if (dist < attackDistance)
+            if (dist <= attackDistance && (!isAttacking || _actionDelay >= attackDuration))
             {
+                anim.applyRootMotion = false;
+                _actionDelay = 0f;
+                attackTimer = 1f;
                 aiState = AIState.attack;
                 attack();
             }
-            */
+            if (dist > attackDistance && (!isAttacking || _actionDelay >= attackDuration))
+            {
+                anim.applyRootMotion = false;
+                aiState = AIState.chasePlayer;
+                chasePlayer();
+            }
+            if (isAttacking)
+            {
+                _actionDelay += Time.deltaTime;
+                attackTimer -= Time.deltaTime;
+                transform.LookAt(player.position);
+            }
         }
 
         void chasePlayer()
         {
-            SetDestinationMoving();
+            anim.SetBool("IsAttacking", false);
+            dot = Vector3.Dot((transform.position- player.position).normalized, (playerVelocityReporter.velocity).normalized);
+            if (dot >= 0.7f)
+            {
+                agent.destination = player.position;
+            }
+            else
+            {
+                SetDestinationMoving();
+            }
+        }
+        
+        void attack()
+        {
+            agent.destination = agent.transform.position;
+            isAttacking = true;
+            anim.applyRootMotion = true;
+            anim.SetBool("IsAttacking", true);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.gameObject.tag == "Player" && attackTimer <= 0.1f && attackTimer >= -0.55f)
+            {
+                Debug.Log("Damage Done to Player");
+            }
         }
 
         private void SetDestinationMoving()
@@ -63,6 +108,17 @@ namespace Enemy
             agent.destination = hit.position;
         }
 
+        public void UpdateAnimClipTimes()
+        {
+            AnimationClip[] clips = anim.runtimeAnimatorController.animationClips;
+            foreach (AnimationClip clip in clips)
+            {
+                if (clip.name == "Luigi_Kick")
+                {
+                    attackDuration = clip.length;
+                }
+            }
+        }
     }
 }
 
