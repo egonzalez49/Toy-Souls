276494ce89c2792e6b5ecb407ff42aeed3eba72e
egonzalez49 <egonzalez49@gatech.edu>
2019-11-20 21:07:53 -0500

    Locked Mouse to Game

--- Assets/Scripts/Managers/GeneralManager.cs
+++ Assets/Scripts/Managers/GeneralManager.cs
@@ -4,22 +4,34 @@
 public class GeneralManager : MonoBehaviour
 {
     public static bool gamePausedOrDone;
+    public static bool lockedMouse;
 
     // Start is called before the first frame update
     void Awake()
     {
         gamePausedOrDone = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        lockedMouse = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Cursor.lockState = lockedMouse ? CursorLockMode.None : CursorLockMode.Locked;
+        Cursor.visible = lockedMouse;
     }
 
     // if game is paused or done
     public static void UpdateGameState(bool state)
     {
+        if (state)
+        {
+            lockedMouse = true;
+        } else
+        {
+            lockedMouse = false;
+        }
         gamePausedOrDone = state;
     }
 }
