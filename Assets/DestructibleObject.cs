0445117f180ac2d75e9581814bb41a96d24a6514
Jiyoung Lim <33502257+sam-lim@users.noreply.github.com>
2019-12-05 16:37:11 -0500

    Destructibles crates are added

---
+++ Assets/DestructibleObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class DestructibleObject : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+
+    private void OnTriggerEnter(Collider col) {
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);       
+    }
+}
