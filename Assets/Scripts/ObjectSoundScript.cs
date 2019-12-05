799267ed281681af1e634f089f9cd96c38921375
egonzalez49 <egonzalez49@gatech.edu>
2019-12-05 14:24:02 -0500

    Sounds to Crates

---
+++ Assets/Scripts/ObjectSoundScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ObjectSoundScript : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude >= 4.2 && !audioSource.isPlaying)
+            audioSource.Play();
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.relativeVelocity.magnitude >= 3 && !audioSource.isPlaying)
+            audioSource.Play();
+    }
+}
