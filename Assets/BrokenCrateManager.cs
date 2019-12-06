79da4de7bbe2cedccc48c4a887b717c029e13046
mliang1987 <mliang@gatech.edu>
2019-12-05 19:11:16 -0500

    Crates make sound on break and disappear.

---
+++ Assets/BrokenCrateManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenCrateManager : MonoBehaviour
+{
+
+    public float max_time;
+    public float start_sink;
+    public float life;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        life = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        life += Time.deltaTime;
+        if (life > start_sink)
+        {
+            Vector3 pos = transform.position;
+            pos.y -= 0.05f;
+            transform.position = pos;
+        }
+        if (life > max_time)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/DestructibleObject.cs
+++ Assets/DestructibleObject.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class DestructibleObject : MonoBehaviour
 {
     public GameObject destroyedVersion;
+    public AudioSource crateAudio;
+
+    private void Awake()
+    {
+        crateAudio = GetComponentInParent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider col) {
+        if(crateAudio != null)
+        {
+            crateAudio.Play();
+        }
         Instantiate(destroyedVersion, transform.position, transform.rotation);
         Destroy(gameObject);       
     }
